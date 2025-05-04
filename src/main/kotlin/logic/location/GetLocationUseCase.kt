2ef5ca5de44477a40d9921fe26f9b06ba2d4ccf2
Eslam <eslam.mohamed.551999@gmail.com>
2025-05-05 01:33:05 +0300

    add location use case implementation

--- src/main/kotlin/logic/location/GetLocationUseCase.kt
+++ src/main/kotlin/logic/location/GetLocationUseCase.kt
@@ -1,5 +1,8 @@
 package logic.location
 
+import logic.exception.InvalidCityNameException
+import logic.exception.InvalidCountryNameException
+import logic.exception.NoLocationFoundException
 import logic.model.Coordinate
 import logic.repository.LocationRepository
 
@@ -7,6 +10,16 @@ class GetLocationUseCase(
     private val locationRepository: LocationRepository
 ) {
     fun getLocation(cityName: String, country: String): Coordinate {
-        TODO()
+        if (!validateCityName(cityName)) throw InvalidCityNameException()
+        if (!validateCountry(country)) throw InvalidCountryNameException()
+        return locationRepository.getLocationByCityAndCountry(cityName, country) ?: throw NoLocationFoundException()
+    }
+
+    private fun validateCityName(cityName: String): Boolean {
+        return cityName.isNotEmpty()
+    }
+
+    private fun validateCountry(country: String): Boolean {
+        return country.isNotEmpty()
     }
 }
