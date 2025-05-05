44bb0c558aa6ab1373d539845f8fa3924bcba483
Amr Ashraf <AmrAshraf_inf@outlook.com>
2025-05-05 21:36:47 +0300

    add initial implementation for the usecase

--- src/main/kotlin/logic/clothesSuggester/SuggestClothesUseCase.kt
+++ src/main/kotlin/logic/clothesSuggester/SuggestClothesUseCase.kt
@@ -1,11 +1,28 @@
 package logic.clothesSuggester
 
 import logic.model.Cloth
+import logic.model.ClothType
 import logic.model.Weather
+import logic.model.WeatherCondition
 import logic.repository.ClothesRepository
 
 class SuggestClothesUseCase(private val clothesRepository: ClothesRepository) {
     suspend fun getSuggestedClothes(weather: Weather): List<Cloth> {
-        TODO()
+        val avgTemp = weather.hourlyTemperatures.map { it.temperature }.averageOrNull()
+
+        val tempBasedType = when {
+            avgTemp == null -> emptyList()
+            avgTemp < 5 -> listOf(ClothType.HEAVY)
+            avgTemp in 5.0..15.0 -> listOf(ClothType.MEDIUM)
+            avgTemp in 15.1..25.0 -> listOf(ClothType.LIGHT)
+            else -> listOf(ClothType.VERY_LIGHT)
+        }
+
+        val types = (tempBasedType).distinct()
+
+        return types.flatMap { clothesRepository.getClothesByType(it) }
     }
+
+    private fun List<Float>.averageOrNull(): Double? =
+        if (isEmpty()) null else this.average()
 }
