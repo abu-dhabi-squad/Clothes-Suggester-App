97a632484024ae6b71d3bf1060bb9848f2f9f75c
NourElhoda Ahmed <nourelhodaahmed208@gmail.com>
2025-05-05 13:14:55 +0300

    create weather mapper implementation test

---
+++ src/test/kotlin/data/weather/repository/mapper/WeatherMapperImplTest.kt
@@ -0,0 +1,27 @@
+package data.weather.repository.mapper
+
+import com.google.common.truth.Truth.assertThat
+import helper.createDtoWeather
+import logic.model.HourlyTemperature
+import logic.model.Weather
+import logic.model.WeatherCondition
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import kotlin.test.Test
+
+class WeatherMapperImplTest{
+    private lateinit var weatherMapper: WeatherMapper
+
+    @BeforeEach
+    fun setup() {
+        weatherMapper = WeatherMapperImpl()
+    }
+
+    @Test
+    fun `mapDtoToWeather should return weather`() {
+        val dtoWeather = createDtoWeather(listOf(HourlyTemperature(10f, 12)), WeatherCondition.WINDY)
+        val expectedWeather = Weather(listOf(HourlyTemperature(10f, 12)), WeatherCondition.WINDY)
+        val actualWeather = weatherMapper.mapDtoToWeather(dtoWeather)
+        assertThat(actualWeather).isEqualTo(expectedWeather)
+    }
+}
