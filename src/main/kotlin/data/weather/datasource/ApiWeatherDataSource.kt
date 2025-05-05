8d662e41bca5cd1da0f0717c56a6cf7982064942
NourElhoda Ahmed <nourelhodaahmed208@gmail.com>
2025-05-05 20:43:13 +0300

    get the hourly forecast for only the current day

--- src/main/kotlin/data/weather/datasource/ApiWeatherDataSource.kt
+++ src/main/kotlin/data/weather/datasource/ApiWeatherDataSource.kt
@@ -11,7 +11,7 @@ import logic.model.Coordinate
 class ApiWeatherDataSource : WeatherDataSource {
     override suspend fun getWeatherByCoordinate(coordinate: Coordinate): DtoWeather {
         val client = HttpClient(CIO)
-        val response = client.get("https://api.open-meteo.com/v1/forecast?latitude=${coordinate.latitude}&longitude=${coordinate.longitude}&daily=weather_code,temperature_2m_max,temperature_2m_min&hourly=temperature_2m,weather_code&current=weather_code,temperature_2m")
+        val response = client.get("https://api.open-meteo.com/v1/forecast?latitude=${coordinate.latitude}&longitude=${coordinate.longitude}&daily=weather_code,temperature_2m_max,temperature_2m_min&hourly=temperature_2m,weather_code&current=weather_code,temperature_2m&forecast_days=1")
         val data = Json.decodeFromString<DtoWeather>(response.bodyAsText())
         return data
     }
