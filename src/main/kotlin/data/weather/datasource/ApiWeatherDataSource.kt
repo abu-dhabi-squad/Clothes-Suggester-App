276176210cc28548f191df11a0fd8b78bb07ceaa
NourElhoda Ahmed <nourelhodaahmed208@gmail.com>
2025-05-05 15:09:07 +0300

    create api weather data source

--- src/main/kotlin/data/weather/datasource/ApiWeatherDataSource.kt
+++ src/main/kotlin/data/weather/datasource/ApiWeatherDataSource.kt
@@ -1,10 +1,18 @@
 package data.weather.datasource
 
 import data.weather.model.DtoWeather
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import kotlinx.serialization.json.Json
 import logic.model.Coordinate
 
 class ApiWeatherDataSource : WeatherDataSource {
     override suspend fun getWeatherByCoordinate(coordinate: Coordinate): DtoWeather {
-        TODO("Not yet implemented")
+        val client = HttpClient(CIO)
+        val response = client.get("https://api.open-meteo.com/v1/forecast?latitude=${coordinate.latitude}&longitude=${coordinate.longitude}&daily=weather_code,temperature_2m_max,temperature_2m_min&hourly=temperature_2m,weather_code&current=weather_code,temperature_2m")
+        val data = Json.decodeFromString<DtoWeather>(response.bodyAsText())
+        return data
     }
 }
