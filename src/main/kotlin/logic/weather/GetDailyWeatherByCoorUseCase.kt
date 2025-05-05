4387e1f53a11029caa1d52c939a78a907e63b826
NourElhoda Ahmed <nourelhodaahmed208@gmail.com>
2025-05-05 12:17:57 +0300

    add get daily weather by coordinate use case implementation

--- src/main/kotlin/logic/weather/GetDailyWeatherByCoorUseCase.kt
+++ src/main/kotlin/logic/weather/GetDailyWeatherByCoorUseCase.kt
@@ -1,13 +1,28 @@
 package logic.weather
 
+import logic.exception.NoHourlyTemperatureFound
+import logic.exception.NoWeatherConditionFound
 import logic.model.Coordinate
+import logic.model.HourlyTemperature
 import logic.model.Weather
+import logic.model.WeatherCondition
 import logic.repository.WeatherRepository
 
 class GetDailyWeatherByCoordinateUseCase(
     private val weatherRepository: WeatherRepository
 ) {
     suspend fun getDailyWeather(coordinate: Coordinate): Weather {
-        TODO()
+        val dailyWeather = weatherRepository.getDailyWeather(coordinate)
+        if (!isValidHourlyWeather(dailyWeather.hourlyTemperatures)) throw NoHourlyTemperatureFound()
+        if (!isValidWeatherCondition(dailyWeather.weatherCondition)) throw NoWeatherConditionFound()
+        return dailyWeather
+    }
+
+    private fun isValidHourlyWeather(hourlyTemperatures: List<HourlyTemperature>): Boolean{
+        return hourlyTemperatures.isNotEmpty()
+    }
+
+    private fun isValidWeatherCondition(weatherCondition: WeatherCondition?): Boolean{
+        return !(weatherCondition == null)
     }
 }
